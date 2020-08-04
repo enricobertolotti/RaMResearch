454437d4e5ad9f96c43486e44b2b38d8a9247cfa
enricobertolotti <e.m.bertolotti@student.utwente.nl>
2020-08-04 11:52:35 +0200

    Removed

--- RaMCode/Analysis/Utils/Generate.py
+++
@@ -1,9 +0,0 @@
-######################################################
-###### Class to automate generation of assets ########
-######################################################
-
-#
-# # Generate all ring images
-# from RaMCode.Data.RingV2 import generate_all_rotations
-#
-# generate_all_rotations(int(55 / 2), 200, anglerange=(90, 180))
